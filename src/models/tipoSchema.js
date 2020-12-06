decc798aea2a6bbc6bd058e402a2a2a16b0da1dc
Renata Abreu <renata89abreu@gmail.com>
2020-12-06 18:28:02 -0300

    remove _id definition and 'nome' field is now unique

--- src/models/tipoSchema.js
+++ src/models/tipoSchema.js
@@ -2,16 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const tipoSchema = new Schema({
-    _id: {
-        type: Schema.Types.ObjectId,
-        unique: true,
-        auto: true,
-        required: true
-    },
     nome: {
         type: String,
-        required: "Este campo é obrigatório."
-
+        required: "Este campo é obrigatório.",
+        unique: true
     }
 })
 
