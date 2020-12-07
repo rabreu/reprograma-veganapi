82d7fca69eb6f073a9eb4f91a78d48116a02ee59
Renata Abreu <renata89abreu@gmail.com>
2020-12-07 13:24:13 -0300

    / is redirected to API_PATH

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 const dotenv = require('dotenv')
 dotenv.config()
 const API_PATH = process.env.API_PATH
 
 router.get('/', (req, res) => {
     console.log(`${req.method} ${API_PATH}${req.url}`);
-    return res.status(200).send("Bem vinde ao VeganAPI 1.0.");
+    return res.status(200).sendFile(path.join(__dirname+'\\index.html'));
 })
 
 module.exports = router
