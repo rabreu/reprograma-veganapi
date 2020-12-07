09e508ad1c98dbe1281f2eb8b68fd6ff642ffa20
Renata Abreu <renata89abreu@gmail.com>
2020-12-07 10:34:21 -0300

    api port moved to env var

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./src/app')
-const { PORT, API_PATH } = require('./src/conf')
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT = process.env.PORT
+const { API_PATH } = require('./src/conf')
+
 
 app.listen(PORT, () => {
     console.log(`VeganAPI server running on http://localhost:${PORT}${API_PATH}.`)
--- src/conf.js
+++ src/conf.js
@@ -1,7 +1,5 @@
 const API_PATH = "/VeganAPI"
-const PORT = 8080
 
 module.exports = {
     API_PATH,
-    PORT,
 }
