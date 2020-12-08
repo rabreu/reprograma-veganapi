d12724922aae5be951bdcbc8ead70d57e3fc9398
Renata Abreu <renata89abreu@gmail.com>
2020-12-07 22:03:02 -0300

    MONGO_URL renamed to MONGO_URI

--- src/confs/database.js
+++ src/confs/database.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const connect = () => {
-    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     const connection = mongoose.connection
     connection.on("error", () => console.log("Couldn't connect to MongoDB."))
     connection.once("open", () => console.log(`Connected to MongoDB on ${connection.host}:${connection.port}.`))
