12b78474a0154b449ef4aaba682e6c2b85531145
Renata Abreu <renata89abreu@gmail.com>
2020-12-08 10:24:23 -0300

    HOST env var is not necessary

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const app = require('./src/app')
 const dotenv = require('dotenv')
 dotenv.config()
-const HOST = process.env.HOST
 const PORT = process.env.PORT
 const API_PATH = process.env.API_PATH
 
 app.listen(PORT, () => {
-    console.log(`VeganAPI server running on http://${HOST}:${PORT}${API_PATH}.`)
+    console.log(`VeganAPI server running on http://localhost:${PORT}${API_PATH}.`)
 })
